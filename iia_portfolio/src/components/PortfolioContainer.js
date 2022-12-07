761d3b482b216f739bececdb2417c0b6f182a654
Squiddy <wolaki96@gmail.com>
2022-12-06 23:00:54 -0500

    added icon

---
+++ iia_portfolio/src/components/PortfolioContainer.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import Nav from './nav';
+import Resume from './pages/resume';
+import About from './pages/about';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
+
+export default function PortfolioContainer() {
+  const [currentPage, setCurrentPage] = useState('About');
+
+  
+  const renderPage = () => {
+    if (currentPage === 'About') {
+      return <About />;
+    }
+    if (currentPage === 'Portfolio') {
+      return <Portfolio />;
+    }
+    if (currentPage === 'Resume') {
+      return <Resume />;
+    }
+    return <Contact />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      {/* // TODO: Add a comment describing what we are passing as props */}
+      <Nav currentPage={currentPage} handlePageChange={handlePageChange} />
+      {/* // TODO: Add a comment explaining what is happening on the following line */}
+      {renderPage()}
+    </div>
+  );
+}
--- iia_portfolio/src/components/nav.jsx
+++ iia_portfolio/src/components/nav.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
 
-const Nav = ({ showAbout, showProjects, showContact, showResume }) => {
+const Nav = ({ showAbout, showPortfolio, showContact, showResume }) => {
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems:
   'center'}}>
     <h1 onClick={showAbout}>About Me</h1>
-    <h1 onClick={showProjects}>Portfolio</h1>
+    <h1 onClick={showPortfolio}>Portfolio</h1>
     <h1 onClick={showContact}>Contact</h1>
     <h1 onClick={showResume}>Resume</h1>
     </div>
