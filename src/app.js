6d9f0b710ff3896fc1119a31348b420fd172fa65
Squiddy <wolaki96@gmail.com>
2022-12-06 21:10:04 -0500

    changed app.js from starter code

--- src/app.js
+++ src/app.js
@@ -1,6 +1,46 @@
-import React from "react";
-import PortfolioContainer from "./components/PortfolioContainer";
+import React from 'react';
+import { useState } from 'react';
+import './App.css';
+import Nav from "./components/HeaderNav";
+import About from "./components/About";
+import Portfolio from "./components/Portfolio";
+import Contact from "./components/Contact";
+import Resume from "./components/Resume";
+import Footer from "./components/Footer";
 
-const App = () => <PortfolioContainer />;
+function App() {
+
+  const [pageIndex, setPageIndex] = useState(0);
+
+  return (
+
+    <div>
+
+      <Nav
+        showAbout={() => setPageIndex(0)}
+        showPortfolio={() => setPageIndex(1)}
+        showContact={() => setPageIndex(2)}
+        showResume={() => setPageIndex(3)}
+        active={pageIndex}
+
+      />
+      {pageIndex === 0 ? (
+        <About />
+      ) : pageIndex === 1 ? (
+        <Portfolio />
+      ) : pageIndex === 2 ? (
+        <Contact />
+      ) : pageIndex === 3 ? (
+        <Resume />
+      ) : (
+        <About />
+      )}
+
+      < Footer />
+
+    </div >
+
+  );
+}
 
 export default App;
